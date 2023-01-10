da6ac2580f5d0625ffe1e6cce041ff8ac235265d
ameneterh <ameneterh@gmail.com>
2023-01-10 10:54:13 +0100

    Update to file path

--- Scripts/index.js
+++ Scripts/index.js
@@ -1,5 +1,5 @@
 var year = new Date().getFullYear();
-document.getElementById("year").innerHTML = year;
+document.getElementById("currentYear").innerHTML = year;
 
 function getDropMenu() {
   var menu = document.getElementById("drop-down-menu");
