689578be2bb34c863854e45d75d97c7ae598c07b
ameneterh <ameneterh@gmail.com>
2023-01-10 01:09:12 +0100

    First commit of HGOS website

---
+++ Scripts/index.js
@@ -0,0 +1,12 @@
+var year = new Date().getFullYear();
+document.getElementById("year").innerHTML = year;
+
+function getDropMenu() {
+  var menu = document.getElementById("drop-down-menu");
+
+  if (menu.style.display == "flex") {
+    menu.style.display = "none";
+  } else {
+    menu.style.display = "flex";
+  }
+}
