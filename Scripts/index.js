f40dbe0ae20965c4b01ae58800002afba4912a35
ameneterh <ameneterh@gmail.com>
2023-01-14 08:55:15 +0100

    Adding documentresources page, editing dropdown

--- Scripts/index.js
+++ Scripts/index.js
@@ -11,12 +11,20 @@ function getDropMenu() {
   }
 }
 
-function getResourcesDropMenu() {
-  var menu = document.getElementById("resources");
+var modal = document.getElementById("tcmodal");
+var span = document.getElementsByClassName("close")[0];
+var butt = document.getElementById("btn");
 
-  if (menu.style.display == "block") {
-    menu.style.display = "none";
-  } else {
-    menu.style.display = "block";
-  }
+function loadtcPage() {
+  modal.style.display = "block";
 }
+
+span.onclick = function () {
+  modal.style.display = "none";
+};
+
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
